77762fa9ac309d80140714f821e73221e94fe7ae
Jason <mail@jasonlambert.co.uk>
2009-08-06 11:46:51 +0100

    add metric fu to default app

--- standard.rb
+++ standard.rb
@@ -318,6 +318,91 @@ namespace :test do
 end
 END
 
+
+file 'lib/tasks/metric_fu.rake', <<-END
+require 'metric_fu'
+
+# Fix NaN issue: http://thestewscope.wordpress.com/2009/07/09/ruby-code-quality-and-metric_fu/
+module MetricFu
+  class Generator
+    def round_to_tenths(decimal)
+      decimal=0.0 if decimal.to_s.eql?('NaN')
+      (decimal.to_i * 10).round / 10.0
+    end
+  end
+end
+
+
+MetricFu::Configuration.run do |config|
+  #define which metrics you want to use
+  config.metrics  = [:churn, :saikuro, :stats, :flog, :flay, :reek, :roodi, :rcov]
+  config.graphs   = [:flog, :flay, :reek, :roodi, :rcov]
+  config.flay     = { :dirs_to_flay => ['app', 'lib']  }
+  config.flog     = { :dirs_to_flog => ['app', 'lib']  }
+  config.reek     = { :dirs_to_reek => ['app', 'lib']  }
+  config.roodi    = { :dirs_to_roodi => ['app', 'lib'] }
+  config.saikuro  = { :output_directory => 'scratch_directory/saikuro',
+                      :input_directory => ['app', 'lib'],
+                      :cyclo => "",
+                      :filter_cyclo => "0",
+                      :warn_cyclo => "5",
+                      :error_cyclo => "7",
+                      :formater => "text"} #this needs to be set to "text"
+  config.churn    = { :start_date => "1 year ago", :minimum_churn_count => 10}
+  config.rcov     = { :test_files => ['test/**/*_test.rb',
+                                      'spec/**/*_spec.rb'],
+                      :rcov_opts => ["--sort coverage",
+                                     "--no-html",
+                                     "--text-coverage",
+                                     "--no-color",
+                                     "--profile",
+                                     "--rails",
+                                     "--exclude /gems/,/Library/,spec",
+                                     "-ltest"]}
+end
+END
+
+
+file 'config/environments/test.rb', <<-END
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The test environment is used exclusively to run your application's
+# test suite.  You never need to work with it otherwise.  Remember that
+# your test database is "scratch space" for the test suite and is wiped
+# and recreated between test runs.  Don't rely on the data there!
+config.cache_classes = true
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
+config.action_view.cache_template_loading            = true
+
+# Disable request forgery protection in test environment
+config.action_controller.allow_forgery_protection    = false
+
+# Tell Action Mailer not to deliver emails to the real world.
+# The :test delivery method accumulates sent emails in the
+# ActionMailer::Base.deliveries array.
+config.action_mailer.delivery_method = :test
+
+# Use SQL instead of Active Record's schema dumper when creating the test database.
+# This is necessary if your schema can't be completely dumped by the schema dumper,
+# like if you have constraints or database-specific column types
+# config.active_record.schema_format = :sql
+
+config.gem 'flay' #, :version => '2.1.0'
+config.gem 'flog'
+config.gem 'reek'
+config.gem 'roodi'
+config.gem 'gruff'
+config.gem 'jscruggs-metric_fu', :lib => 'metric_fu', :source => 'http://gems.github.com'
+END
+
+rake("gems:install RAILS_ENV=test", :sudo => true)
+
 # add to Git
 git :init
 git :add => "."
